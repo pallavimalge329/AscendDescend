f3b3bd91b3698bbd5eb75cde36aec6091d1108f6
HP <HP@DESKTOP-N5QBMVO>
2021-09-01 14:09:54 +0530

    Add project files.

---
+++ AscendDescend/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AscendDscend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 9, 6, 7, 5, 9 };
+
+            // Sort array in ascending order.
+            Array.Sort(arr);
+
+            // reverse array
+            Array.Reverse(arr);
+
+            // print all element of array
+            Console.Write("Array Element in Decending Order:\n ");
+
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+            Console.Write("\n\n");
+
+
+
+            Array.Reverse(arr);
+            Console.Write("Array Element in Decending Order:\n ");
+            // print all element of array
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+            Console.Read();
+            Console.Read();
+        }
+
+
+
+    }
+}
